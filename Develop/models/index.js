b02d6ad7b32c6654462a20d7e7cb2270c8e725e8
michaelshxw <michaelshxw@gmail.com>
2021-06-07 13:14:13 +0800

    add foreign keys to index.js files

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,29 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { BelongsTo } = require('sequelize/types');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: productTag,
+  foreignKey: "product_id"
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: productTag,
+  foreignKey: "product_id"
+})
 
 module.exports = {
   Product,
