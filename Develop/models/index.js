f1e975ca19c86d6fec7373f2dc4a628b48528cd3
michaelshxw <michaelshxw@gmail.com>
2021-06-09 22:59:52 +0800

    add foreign keys

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,24 +6,24 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: "category_id"
+  foreignKey: "category_id",
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: "category_id"
+  foreignKey: "category_id",
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: "productTag",
-  foreignKey: "product_id"
+  foreignKey: "product_id",
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: "productTag",
-  foreignKey: "product_id"
+  foreignKey: "product_id",
 })
 
 module.exports = {
